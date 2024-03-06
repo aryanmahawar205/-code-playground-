142b006f09e49132f3184d6d7640e488daff88c3
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-06 23:35:10 +0530

    modidified temp.c

--- temp.c
+++ temp.c
@@ -1,80 +1,10 @@
-//this code will ask the user to enter a sentence which will be passed to a function that detects and prints out the longest word in
-//that sentence
-//eg, if sentence is - "This is real mesmerization", then the output should be "mesmerization" is the longest word
+//place to test out logic for code
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char* largestWord(char *s);
 
 int main()
 {
-    char s[] = "This is real mesmerization.";
+  printf("\nHello World\n\n");
 
-    char *word = largest_word(s);
-
-    printf("%s\n", word);
-  
-    free(word);
-  
-    return 0;
-}
-
-char* largestWord(char *s)
-{
-    int len = strlen(s);
-    int count = 0;
-    int max = 0;
-
-    char buffer_word[300];
-    char max_word[300];
-
-    char delimiters[] = " .,;\n\t";
-  
-    int i = 0;
-    while (i < len)
-    {
-      count = 0;
-      while (i < len)
-      {
-        if (strchr(delimiters, s[i]) != NULL)
-        {
-            break;
-        }
-      
-        else
-        {
-            buffer_word[count] = s[i];
-            i++;
-            count++;
-        }
-      }
-
-      buffer_word[count] = '\0';
-
-      if (count > max)
-      {
-        max = count;
-        strcpy(max_word, buffer_word);
-      }
-    
-      while (i < len)
-      {
-        if (strchr(delimiters, s[i]) == NULL)
-        {
-          break;
-        }
-        else
-        {
-          i++;
-        }
-      }
-    }
-
-    char *word;
-    word = (char *)malloc(sizeof(char)*(max + 1));
-    strcpy(word, max_word);
-
-    return word;
+  return 0;
 }
