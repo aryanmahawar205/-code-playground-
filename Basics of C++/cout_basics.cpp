fef1a1d7b5de8150542e40ae4ac4756164e86e30
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-08 18:22:58 +0530

    added cout basics file

---
+++ Basics of C++/cout_basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    cout << endl; //gives newline, equivalent of using /n
+
+    cout << "ABC";
+    cout << "abc";
+
+    cout << "ABC" << endl;
+    cout << "abc" << endl;
+
+    int x = 20;
+    cout << "x - " << x << endl;
+
+    int age = 45;
+    cout << "He is - " << age << " years old" << endl;
+
+    return 0;
+}
