997ff4de958812e024bb64bbdb3ed1ff5d3df64b
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-08 14:38:22 +0530

    modified temp.c

--- temp.c
+++ temp.c
@@ -1,10 +1,19 @@
 //place to test out logic for code
 
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-  printf("\nHello World\n\n");
+  char str[] = "The sun is yellow";
+
+  char *last = strrchr(str, ' ');
+  
+  printf("%s\n", last+1);
+
+  int length = strlen(last+1);
+
+  printf("%d\n", length);
 
   return 0;
 }
