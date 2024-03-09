277c9220f9d4ccc069158d9e9613bbffc798b17d
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-09 17:52:11 +0530

    increased code modularity

--- Digital Assignment 1/matrix_addition_using_pointers.c
+++ Digital Assignment 1/matrix_addition_using_pointers.c
@@ -3,11 +3,11 @@
 
 #include <stdio.h>
 
-int rows, cols;
+int rows, cols; //declaring global variables for rows and columns to be accessible for all functions below
 
-void inputMatrix(int mat[rows][cols]);
-void printMatrix(int mat[rows][cols]);
-void addMatrix(int mat1[rows][cols], int mat2[rows][cols], int sum[rows][cols]);
+void inputMatrix(int mat[rows][cols]); //function to take matrix input using pointers
+void printMatrix(int mat[rows][cols]); //function to print the matrix using pointers
+void addMatrix(int mat1[rows][cols], int mat2[rows][cols], int sum[rows][cols]); //function to add the two matrices using pointers
 
 int main()
 {
@@ -17,34 +17,36 @@ int main()
     printf("Enter the number of columns you wish to have in the matrix - ");
     scanf("%d", &cols);
 
-    int mat1[rows][cols], mat2[rows][cols], sum[rows][cols];
+    int mat1[rows][cols], mat2[rows][cols], sum[rows][cols]; //declaring all required matrices
 
     printf("\nEnter the elements of the first matrix - \n");
-    inputMatrix(mat1);
+    inputMatrix(mat1); //taking input for first matrix elements
 
     printf("\nEnter the elements of the second matrix - \n");
-    inputMatrix(mat2);
+    inputMatrix(mat2); //taking input for second matrix elements
 
-    addMatrix(mat1, mat2, sum);
+    addMatrix(mat1, mat2, sum); //computing the addition of the two matrices into matrix sum
 
     printf("\nFirst matrix inputted as - \n");
-    printMatrix(mat1);
+    printMatrix(mat1); //printing the first matrix
 
     printf("\nSecond matrix inputted as - \n");
-    printMatrix(mat2);
+    printMatrix(mat2); //printing the second matrix
 
     printf("\nSum matrix computed as (by using pointers) - \n");
-    printMatrix(sum);
+    printMatrix(sum); //printing the sum of the two matrices
 
     return 0;
 }
 
+//function definition for taking input of the matrix using pointers
 void inputMatrix(int mat[rows][cols])
 {
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
+            //the statement &mat[i][j] is equivalent to (*(mat + i) + j)
             scanf("%d", (*(mat + i) + j));
         }
     }
@@ -56,7 +58,7 @@ void printMatrix(int mat[rows][cols])
     {
         for (int j = 0; j < cols; j++)
         {
-            printf("%d ", *(*(mat + i) + j));
+            printf("%d ", *(*(mat + i) + j)); //deference to get the value from the matrix
         }
         printf("\n");
     }
@@ -68,6 +70,7 @@ void addMatrix(int mat1[rows][cols], int mat2[rows][cols], int sum[rows][cols])
     {
         for (int j = 0; j < cols; j++)
         {
+            //deference to get the value from the matrix and store it in sum matrix also by dereferencing
             *(*(sum + i) + j) = *(*(mat1+ i) + j) + *(*(mat2 + i) + j);
         }
     }
