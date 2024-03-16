b4d934c7d6a94dcc31e3c147802e31697ebeb63f
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-16 22:08:44 +0530

    adding constructor file

---
+++ Constructors in C++/basics_of_constructors.cpp
@@ -0,0 +1,43 @@
+//a constructor is a special function that gets called when you create an object for a class
+//remember that constructor function names are the same as the class name in which they are defined
+
+#include <iostream>
+
+using namespace std;
+
+class Book
+{
+    public:
+        string name;
+        string author;
+        int pages;
+
+        Book(string aName, string aAuthor, int aPages) //constructor declared
+        {
+            name = aName;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+        Book()
+        {
+            name = "No name declared";
+            author = "No author declared";
+            pages = 0;
+        }
+};
+
+int main()
+{
+    Book b1("Harry Potter", "JK Rowling", 500);
+
+    Book b2("Lord of the Rings", "Dan Brown", 700);
+
+    cout << b2.author << endl;
+
+    Book b3;
+
+    cout << b3.name;
+
+    return 0;
+}
