1c3a857e31fcb4804168f15f47ca4ac043cca1f7
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-16 22:45:56 +0530

    getters and setters:

---
+++ Getters and Setters in C++/basics_of_getters_and_setters.cpp
@@ -0,0 +1,50 @@
+//getters and setters in classes allow you to control the access to different attributes and elements in the class
+//public keyword in class --> means any code in any class or main function can access it
+//private keyword in class --> only code in that particular class can access it
+
+#include <iostream>
+
+using namespace std;
+
+class Movie
+{
+    private:
+        string rating;
+    public:
+        string title;
+        string director;
+
+        Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            setRating(aRating);
+        }
+
+        void setRating(string aRating)
+        {
+            if (aRating == "G" || aRating == "PG-13" || aRating == "PG" || aRating == "R" || aRating == "NR")
+            {
+                rating = aRating;
+            }
+            else
+            {
+                rating = "NR";
+            }
+        }
+
+        string getRating()
+        {
+            return rating;
+        }
+};
+
+int main()
+{
+    Movie avengers("The Avengers", "Joss Wanton", "PG-13");
+    cout << avengers.getRating() << endl;
+    avengers.setRating("Dog");
+    cout << avengers.getRating() << endl;
+
+    return 0;
+}
