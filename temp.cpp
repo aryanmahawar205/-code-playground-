d7bc69b2cc2c64f001e834e1ea6b2f131fc36284
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-25 21:50:44 +0530

    modifying temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -1,2 +1,21 @@
 //place to test out logic for code
 
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+inline void rhombus(int n)
+{
+    for (int i = n-1; i >= 0; i--)
+    {
+        cout << setw(i+n) << "*****" << endl;
+    }
+}
+
+int main()
+{
+    rhombus(5);
+
+    return 0;
+}
