247aea1b7a8711af1c938e764ce54f407e6590d7
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-03-31 18:54:13 +0530

    setbase() stream manipulation method

---
+++ Stream Manipulators in C++/setbase_stream_manipulator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// setbase is in the iomanip library, so we must include it to use it
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int number = 17;
+
+  // normally integers will output in base 10, i.e. decimal, the way numbers
+  // are typically represented
+  cout << "number: " << number << endl;
+
+  // setbase is a stream manipulator, if we pass in the value 8 using it as
+  // below future ints output in the stream will be output as base 8 (octal)
+  // numbers
+  cout << "number: " << setbase(8) << number << endl;
+
+  // if we pass in the value 16 subsequent integers will be output using base
+  // 16, i.e. hexadecimal numbers
+  cout << "number: " << setbase(16) << number << endl;
+
+  // we can switch it back to base 10 / decimal with setbase(10)
+  cout << "number: " << setbase(10) << number << endl << endl;
+
+  // output the numbers 0-32 as decimal, octal and hexadecimal numbers
+  for (int i = 0; i <= 32; i++)
+  {
+    cout << setbase(10) << i << " ";
+    cout << setbase(8) << i << " ";
+    cout << setbase(16) << i << endl;
+  }
+
+  return 0;
+}
