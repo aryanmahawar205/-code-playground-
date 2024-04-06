e4716a843ec8cb3606d13717bec8ee4a9073aa94
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-04-06 18:02:56 +0530

    adding comments

--- Digital Assignment 2/function_overriding.cpp
+++ Digital Assignment 2/function_overriding.cpp
@@ -1,45 +1,53 @@
-//in this code we will be creating a base class called Book and will inherit it to 2 other derived classes called Reference_Book and Magazine
+//in this code we will be creating a base class called Book and will inherit it to 2 other derived classes namely Reference_Book and Magazine
 //the base class Book will have a function called display which essentially displays the details of the book
 //each of the derived classes will have the same function too but in the derived classes, the function will perform slightly
 //different tasks thus achieving the concept of function overriding in C++ inheritance
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+//defining base class Book
 class Book
 {
     public:
         string name, author;
 
+        //function to set up the book name and book author
         void getData(string aName, string aAuthor)
         {
             name = aName;
             author = aAuthor;
         }
 
+        //display function which will get overridden in the upcoming derived classes
         void display()
         {
-            cout << "display function of the base class Book has been invoked" << endl;
+            cout << "~~~~~display function of the base class Book has been invoked~~~~~" << endl;
         }
 };
 
+//first derived class called Reference_Book
 class Reference_Book : public Book
 {
     public:
+        //overriding display function in derived class Reference_Book
         void display()
         {
-            cout << "display function of the derived class Reference_Book has been invoked (function overriding)" << endl << endl;
+            cout << "~~~~~display function of the derived class Reference_Book has been invoked (function overriding)~~~~~" << endl << endl;
             cout << "Your book is " << name << " by " << author << endl << endl;
         }
 };
 
+//second derived class class Magazine
 class Magazine : public Book
 {
     public:
+        //overriding display function in derived class Magazine
         void display()
         {
-            cout << "display function of the derived class Magazine has been invoked (function overriding)" << endl << endl;
+            cout << "~~~~~~display function of the derived class Magazine has been invoked (function overriding)~~~~~" << endl << endl;
             cout << "Your book is " << name << " by " << author << endl << endl;
         }
 };
@@ -50,7 +58,7 @@ int main()
     cout << endl;
     cout << "What type of Book object you wish to create? (Enter 1 for a Reference Book or 2 for a Magazine) - ";
     cin >> type;
-    cin.clear(); cin.sync();
+    cin.clear(); cin.sync(); //clearing the input stream to use getline() later on in the code
     cout << endl;
 
     if (type == 1)
@@ -66,7 +74,7 @@ int main()
 
         cout << endl;
 
-        rb.getData(referenceBookName, referenceBookAuthor); rb.display();
+        rb.getData(referenceBookName, referenceBookAuthor); rb.display(); //function calls through Reference_Book object rb
     }
 
     else if (type == 2)
@@ -81,7 +89,7 @@ int main()
 
         cout << endl;
 
-        m.getData(magazineName, magazineAuthor); m.display();
+        m.getData(magazineName, magazineAuthor); m.display(); //function calls through Magazine object m
     }
 
     else
