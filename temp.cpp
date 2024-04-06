b0af406c2ef64e251615146e765edc496cd39bac
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-04-06 16:58:07 +0530

    modifying temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-inline void rhombus(int n)
-{
-    for (int i = n-1; i >= 0; i--)
-    {
-        cout << setw(i+n) << "*****" << endl;
-    }
-}
-
 int main()
 {
-    rhombus(5);
+    string name, address;
 
-    return 0;
+    cout << "Enter your name - ";
+    getline(cin, name);
+
+    cout << "Enter your address - ";
+    getline(cin, address);
+
+    cout << name << endl;
+    cout << address << endl;
+
 }
