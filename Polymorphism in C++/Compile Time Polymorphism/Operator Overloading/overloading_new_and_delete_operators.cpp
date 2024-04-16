ae85400842597e49b85dbfbcc0d6eb5597003f69
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-04-16 20:52:49 +0530

    adding comments

--- Polymorphism in C++/Compile Time Polymorphism/Operator Overloading/overloading_new_and_delete_operators.cpp
+++ Polymorphism in C++/Compile Time Polymorphism/Operator Overloading/overloading_new_and_delete_operators.cpp
@@ -35,6 +35,7 @@ class Student
             return pointer;
         }
 
+        //overloading delete operator
         void operator delete (void *objectPointer)
         {
             free(objectPointer);
