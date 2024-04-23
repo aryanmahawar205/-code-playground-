44d5d34c99154386be1515f7966d0c9f3825699f
Aryan Mahawar <aryanmahawar205@gmail.com>
2024-04-23 17:43:56 +0530

    dynamic binding theory

---
+++ Polymorphism in C++/Run Time Polymorphism/dynamic_binding.cpp
@@ -0,0 +1,12 @@
+//dynamic binding is performed during run time of the program
+//it is also called Late Binding or Run Time Binding
+//can be achieved by Function Overriding, Virtual Functions, etc - Run Time Polymorphism
+
+//in this case the function bindings is decided during run time by the compiler owing to the functioning of the 'virtual' keyword
+
+int main()
+{
+    //no code statements
+
+    return 0;
+}
